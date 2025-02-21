641353ed6e2ad7902290d5ce55151e29ddb79a5e
Iván López Hdez <ivanlhz@gmail.com>
2025-02-21 09:56:19 +0000

    Fix: cookies

--- config/server.ts
+++ config/server.ts
@@ -1,13 +1,17 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
   },
-  url: env('URL', `https://${env('RAILWAY_STATIC_URL')}`),
-  proxy: true,
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+  url: env('PUBLIC_URL', `https://${env("HOST", "0.0.0.0")}`),
+  cookie: {
+    secure: process.env.NODE_ENV === 'production', // Cookies seguras solo en producción
+    sameSite: 'lax' // Política de sameSite para seguridad
   },
   server: {
     cors: {
